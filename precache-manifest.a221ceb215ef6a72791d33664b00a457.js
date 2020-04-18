23330acbd671a5744bca66f5e56ebdd88068c0a1
Wiktor Szlegier <22wiktors@gmail.com>
2020-04-18 15:30:44 +0200

    Updates

---
+++ precache-manifest.a221ceb215ef6a72791d33664b00a457.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe19c3dabd6410df6a7aa1f6a1119f99",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "08f1a75c220a351cf1d6",
+    "url": "/WeatherApp/static/js/2.fc357f68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeatherApp/static/js/2.fc357f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bd7880ca5dc33059f6",
+    "url": "/WeatherApp/static/js/main.4bb45893.chunk.js"
+  },
+  {
+    "revision": "4583da910e0b66774bc3",
+    "url": "/WeatherApp/static/js/runtime-main.795e39ca.js"
+  },
+  {
+    "revision": "18ae8b6a5ffd4e0f90940179de8a3b80",
+    "url": "/WeatherApp/static/media/clear-day.18ae8b6a.svg"
+  },
+  {
+    "revision": "abd974161640021b8ff745d1918af418",
+    "url": "/WeatherApp/static/media/clear-night.abd97416.svg"
+  },
+  {
+    "revision": "6dfb3b610f1c3c27364dadadf29ef2fe",
+    "url": "/WeatherApp/static/media/cloudy.6dfb3b61.svg"
+  },
+  {
+    "revision": "57afbca9df8e5ca1da0e0dc41e090d64",
+    "url": "/WeatherApp/static/media/fog.57afbca9.svg"
+  },
+  {
+    "revision": "85b6d51b9d624f1b9e91148da470c473",
+    "url": "/WeatherApp/static/media/hail.85b6d51b.svg"
+  },
+  {
+    "revision": "51461db43ab7362a8c5f3dedc920e05a",
+    "url": "/WeatherApp/static/media/partly-cloudy-day.51461db4.svg"
+  },
+  {
+    "revision": "33e7fec5a07ce4c1526dd89625ad7acd",
+    "url": "/WeatherApp/static/media/partly-cloudy-night.33e7fec5.svg"
+  },
+  {
+    "revision": "2cf3bb1c2e51770601dbc9acf4da75ac",
+    "url": "/WeatherApp/static/media/rain.2cf3bb1c.svg"
+  },
+  {
+    "revision": "3a43d65a258977d3dcec3f876c9934d8",
+    "url": "/WeatherApp/static/media/refresh.3a43d65a.svg"
+  },
+  {
+    "revision": "2be9bd0f92b28df3ed2b4ef589ddd85a",
+    "url": "/WeatherApp/static/media/sleet.2be9bd0f.svg"
+  },
+  {
+    "revision": "74f45bc4dfa824ddd037eb0b1f753149",
+    "url": "/WeatherApp/static/media/snow.74f45bc4.svg"
+  },
+  {
+    "revision": "d88b95fa163518910745d7de76b37378",
+    "url": "/WeatherApp/static/media/thunderstorm.d88b95fa.svg"
+  },
+  {
+    "revision": "e977faa6c2d9b406aa98ab80eb66e552",
+    "url": "/WeatherApp/static/media/wind.e977faa6.svg"
+  }
+]);
